1e7d639b0ea368b1e09b9fdb73784ec6dcff51eb
Alejandro E. Losada <alosada@gmail.com>
2016-10-09 15:42:44 -0500

    mass mailer

--- app/admin/guest.rb
+++ app/admin/guest.rb
@@ -5,6 +5,18 @@ ActiveAdmin.register Guest do
 #
 permit_params :full_name, :email, :plus
 
+  collection_action :send_invites, method: :post do
+    collection.each do |guest|
+      BaseMailer.test(guest).deliver_now
+    end
+    redirect_to :back, notice: "emails sent!"
+  end
+
+  action_item only: :index do
+    link_to 'Send invites!', send_invites_admin_guests_path, method: :post
+  end
+
+
 form do |f|
   inputs 'Details' do
     input :email
--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -3,20 +3,9 @@ class BaseMailer < ActionMailer::Base
  
   default from: 'no-reply@aleyaudrey.com'
  
-  def welcome_email(user)
-    mail(to: user.email, subject: "Welcome!")
+  def test(guest)
+    @guest = guest
+    mail(to: @guest.email, subject: "Welcome!")
   end
 
-  def test_api
-    api = 'key-24fd6ca31a5b16160796a3ca4de2be98'
-    dom = 'appddd01dc7e1c44ac6b2778f374c67da29.mailgun.org'
-    url = "https://api:#{api}@api.mailgun.net/v3/#{dom}/messages"
-
-    RestClient.post url,
-      :from => "Excited User <noreply@voyono.com>",
-      :to => "Bob <alosada@gmail.com>",
-      :subject => "This is subject",
-      :text => "Text body",
-      :html => "<b>HTML</b> version of the body!"
-  end
 end
